07c31657e16a431a269a181715b936978e5099f0
IsAurora6 <85173010+IsAurora6@users.noreply.github.com>
2025-05-28 23:53:15 +0800

    feat: expose HTTP methods for invoke-contract and query-contract

---
+++ controllers/chain.go
@@ -0,0 +1,67 @@
+// Copyright 2025 The Casibase Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package controllers
+
+import (
+	"encoding/json"
+
+	"github.com/casibase/chainmaker-server/object"
+)
+
+// InvokeContract
+// @Title InvokeContract
+// @Description invoke contract
+// @Param body object.Chainmaker true "contract invoke parameters"
+// @Success 200 {object} object.Response
+// @Failure 400 Invalid request
+// @router /invoke-contract [post]
+func (c *ApiController) InvokeContract() {
+	var chainmakerInfo object.ChainmakerInfo
+	err := json.Unmarshal(c.Ctx.Input.RequestBody, &chainmakerInfo)
+	if err != nil {
+		c.ResponseError(err.Error())
+		return
+	}
+	chainmakerTxInfo, err := object.InvokeContract(chainmakerInfo)
+	if err != nil {
+		c.ResponseError(err.Error())
+		return
+	}
+
+	c.ResponseOk(chainmakerTxInfo)
+}
+
+// QueryContract
+// @Title QueryContract
+// @Description query contract
+// @Param body object.Chainmaker true "contract query parameters"
+// @Success 200 {object} object.Response
+// @Failure 400 Invalid request
+// @router /query-contract [post]
+func (c *ApiController) QueryContract() {
+	var chainmakerInfo object.ChainmakerInfo
+	err := json.Unmarshal(c.Ctx.Input.RequestBody, &chainmakerInfo)
+	if err != nil {
+		c.ResponseError(err.Error())
+		return
+	}
+	chainmakerTxInfo, err := object.QueryContract(chainmakerInfo)
+	if err != nil {
+		c.ResponseError(err.Error())
+		return
+	}
+
+	c.ResponseOk(chainmakerTxInfo)
+}
---
+++ main.go
@@ -0,0 +1,43 @@
+// Copyright 2025 The Casibase Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/beego/beego"
+	"github.com/beego/beego/plugins/cors"
+	_ "github.com/casibase/chainmaker-server/routers"
+	"github.com/casibase/chainmaker-server/util"
+)
+
+func main() {
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "X-Requested-With", "Content-Type", "Accept"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
+	port := beego.AppConfig.DefaultInt("httpport", 14000)
+
+	err := util.StopOldInstance(port)
+	if err != nil {
+		panic(err)
+	}
+
+	beego.Run(fmt.Sprintf(":%v", port))
+}
